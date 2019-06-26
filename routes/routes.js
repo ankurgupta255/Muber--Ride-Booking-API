687bd5d66a0fa48e5e294d86997ba9d8a9569c95
Ankur Gupta <gupta.ankur255@gmail.com>
2019-06-27 00:59:54 +0530

    Create routes.js

---
+++ routes/routes.js
@@ -0,0 +1,13 @@
+const DriversController=require('../controllers/drivers_controllers');
+
+module.exports=(app)=>{
+	app.get('/api',DriversController.greeting);
+
+	app.post('/api/drivers',DriversController.create);
+
+	app.put('/api/drivers/:id',DriversController.edit);
+
+	app.delete('/api/drivers/:id',DriversController.remove);
+
+	app.get('/api/drivers',DriversController.index);
+};
